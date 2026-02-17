c81068a95430302bd00893b65f3885f3dedd04df
pedrofm <pedrofm@pedrofm.dev>
2026-02-17 18:49:16 +0100

    New release

---
+++ dist/modules/run/components/shared/parts_table/runPartsQrFlow.d.ts
@@ -0,0 +1,14 @@
+import { default as React } from 'react';
+import { Run, RunStep, RunStepPart, RunPart } from '@jield/solodb-typescript-core';
+type Props = {
+    run: Run;
+    runStep: RunStep;
+    runStepParts?: RunStepPart[];
+    runParts?: RunPart[];
+    refetchFn?: () => void;
+    toggleRunPartRef?: React.RefObject<{
+        setPart: (part: number) => void;
+    } | null>;
+};
+declare const RunPartsQrFlow: ({ run, runStep, runStepParts, runParts, refetchFn, toggleRunPartRef, }: Props) => import("react/jsx-runtime").JSX.Element;
+export default RunPartsQrFlow;
