826a321d29d8a7bc0fcab6fa27c75aecbd031f6d
pedrofm <pedrofm@pedrofm.dev>
2026-03-16 16:50:40 +0100

    Show better ux feedback in selectRunWithQrScanner

--- src/modules/run/components/shared/qr-scanner/selectRunWithQrScanner.tsx
+++ src/modules/run/components/shared/qr-scanner/selectRunWithQrScanner.tsx
@@ -1,7 +1,8 @@
 import { makeKeySequenceListener } from "@jield/solodb-react-components/utils/keySequenceListener";
+import Notification, { type NotificationType } from "@jield/solodb-react-components/utils/notification";
 import { Run } from "@jield/solodb-typescript-core";
 import { QRCodeSVG } from "qrcode.react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function NavigateInRunWithQrScanner({
   runsList,
@@ -10,24 +11,45 @@ export default function NavigateInRunWithQrScanner({
 }: {
   runsList: Run[];
   setRun: (run: Run) => void;
-  setRunStepPartId?: (stepPart: number) => void;
-  setRunPartId?: (part: number) => void;
   setRunPartLabel?: (label: string) => void;
 }) {
+  const [readedKeys, setReadedKeys] = useState<string>("");
+  const [notification, setNotification] = useState<NotificationType>({ text: "", show: false, variant: "success" });
+
   const setByPB = (readed: string) => {
-      const runPartBadgeParsed = readed.split("/pb/")[1].split("-");
-      if (!(runPartBadgeParsed.length == 4 || runPartBadgeParsed.length == 3)) return;
+    const runPartBadgeParsed = readed.split("-");
+    if (!(runPartBadgeParsed.length == 4 || runPartBadgeParsed.length == 3)) {
+      setNotification({ text: "Run not found", show: true, variant: "danger" });
+      return;
+    }
 
-      const foundRun = runsList.find((run) => run.label === `${runPartBadgeParsed[0]}-${runPartBadgeParsed[1]}`);
-      const foundRunPartLabel = (`${runPartBadgeParsed[2]}` + (runPartBadgeParsed[3] ? `-${runPartBadgeParsed[3]}` : "")).replace('/', '');
+    const foundRun = runsList.find((run) => run.label === `${runPartBadgeParsed[0]}-${runPartBadgeParsed[1]}`);
+    const foundRunPartLabel = (
+      `${runPartBadgeParsed[2]}` + (runPartBadgeParsed[3] ? `-${runPartBadgeParsed[3]}` : "")
+    ).replace("/", "");
 
-      if (setRun !== undefined && foundRun) setRun(foundRun);
-      if (setRunPartLabel !== undefined && foundRunPartLabel) setRunPartLabel(foundRunPartLabel);
+    if (!foundRun) {
+      setNotification({ text: "Run not found", show: true, variant: "danger" });
+      return;
+    }
+
+    if (!foundRunPartLabel) {
+      setNotification({ text: "Part label not found", show: true, variant: "danger" });
+      return;
+    }
+
+    if (setRun !== undefined) {
+        setNotification({ text: `Found run ${foundRun.label}`, show: true, variant: "success" });
+        setRun(foundRun);
+    }
+    if (setRunPartLabel !== undefined) setRunPartLabel(foundRunPartLabel);
   };
 
   // With document listener for keys
   useEffect(() => {
-    const listener = makeKeySequenceListener("/pb/*/", setByPB);
+    const listener = makeKeySequenceListener("*", setByPB, (keys) => {
+      setReadedKeys(keys);
+    });
 
     document.addEventListener("keyup", listener);
 
@@ -40,8 +62,9 @@ export default function NavigateInRunWithQrScanner({
     <div className="d-flex flex-row gap-3">
       <div className="d-flex flex-column">
         <QRCodeSVG value={"start-form"} size={100} className={"float-end"} onClick={() => {}} />
-        <span className={"text-muted"}>Start QR</span>
+        <span className={"text-muted"}>Reading: {readedKeys}</span>
       </div>
+      <Notification notification={notification} setNotification={setNotification} />
     </div>
   );
 }
--- src/utils/keySequenceListener.ts
+++ src/utils/keySequenceListener.ts
@@ -11,32 +11,50 @@ function isValidCharacter(char: string): boolean {
   return /^[a-zA-Z0-9-]$/.test(char);
 }
 
+function isEndCharacter(char: string): boolean {
+  switch (char) {
+    case "Enter":
+      return true;
+
+    default:
+      return false;
+  }
+}
+
 /**
  * Represents a book.
  * key sequence suports := ([a-zA-Z0-9-], * , /)
- * Never have as last character *
  */
-export function makeKeySequenceListener(keySequence: string, callback: (readedKeys: string) => void) {
-  if (keySequence[keySequence.length - 1] === "*") {
-    throw new Error("Key sequence can not end with a wildcard character");
-  }
-
+export function makeKeySequenceListener(
+  keySequence: string,
+  callback: (readedKeys: string) => void,
+  currentReadedKeys?: (keys: string) => void
+) {
   let i = 0;
   let keys = "";
+
+  const restart = () => {
+    i = 0;
+    keys = "";
+    if (currentReadedKeys) currentReadedKeys(keys);
+  };
 
   return (e: KeyboardEvent) => {
     const key = e.key;
 
     if (isIgnorableKey(key)) return;
+    if (isEndCharacter(key)) {
+      callback(keys);
+      restart();
+      return;
+    }
 
     keys += key;
-    console.log(key);
 
     switch (keySequence[i]) {
       case "/":
         if (key != "/") {
-          i = 0;
-          keys = "";
+          restart();
           return;
         } else {
           i = (i + 1) % keySequence.length;
@@ -49,8 +67,7 @@ export function makeKeySequenceListener(keySequence: string, callback: (readedKe
         }
 
         if (!isValidCharacter(key)) {
-          i = 0;
-          keys = "";
+          restart();
           return;
         }
 
@@ -60,8 +77,7 @@ export function makeKeySequenceListener(keySequence: string, callback: (readedKe
           throw new Error("Invalid character expresion in makeKeySequenceListener call");
         }
         if (key != keySequence[i]) {
-          i = 0;
-          keys = "";
+          restart();
           return;
         } else {
           i = (i + 1) % keySequence.length;
@@ -71,8 +87,10 @@ export function makeKeySequenceListener(keySequence: string, callback: (readedKe
 
     if (i === keySequence.length) {
       callback(keys);
-      i = 0;
-      keys = "";
+      restart();
+      return;
     }
+
+    if (currentReadedKeys) currentReadedKeys(keys);
   };
 }
