3992f83f5842d338e87ce4918e858e760fbff4b9
pedrofm <pedrofm@pedrofm.dev>
2026-03-17 19:20:14 +0100

    New v.1.0.22

--- dist/modules/iris/components/irisOperatorEventDetailsUtils.d.ts
+++
@@ -1,9 +0,0 @@
-export type IrisOperatorEventAction = "approve" | "fail" | "finish" | "reject";
-export interface IrisOperatorEventActionConfig {
-    action: IrisOperatorEventAction;
-    buttonClassName: string;
-    label: string;
-    successMessage: string;
-}
-export declare function getAllowedActionsForState(state: string): IrisOperatorEventActionConfig[];
-export declare function getErrorMessage(error: unknown): string;
